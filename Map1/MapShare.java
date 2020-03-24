ff2dea0b3ef7c9cc9b7c2f1d27b1dbee9b10b225
katemalets <katyamask@gmail.com>
2020-03-24 17:07:28 +0300

    mapshare

---
+++ Map1/MapShare.java
@@ -0,0 +1,37 @@
+package bat.coding.java.Map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapShare {
+    public static void main(String[] args) {
+        Map<String,String> dict = new HashMap<>();
+        dict.put("a","candy");
+        dict.put("b","dirt");
+        dict.put("c","deleeeete");
+        dict.put("d","dvalue");
+        mapShare(dict);
+        for(Map.Entry<String,String> word : dict.entrySet()){
+            System.out.println("Key : " + word.getKey()+ ", value : " + word.getValue());
+        }
+    }
+
+    public static Map<String, String> mapShare(Map<String, String> map) {
+       if(map.containsKey("a")){
+           map.put("b", map.get("a"));
+       }
+       map.remove("c");
+        return map;
+    }
+}
+
+/**
+ * Modify and return the given map as follows: if the key "a" has a value,
+ * set the key "b" to have that same value. In all cases remove the key "c",
+ * leaving the rest of the map unchanged.
+ *
+ *
+ * mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+ * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+ * mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+ */
