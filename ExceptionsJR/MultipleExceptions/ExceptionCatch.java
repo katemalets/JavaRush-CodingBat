57472ab64bae4c29dde5fddc5b4e34e5b30d30ab
katemalets <katyamask@gmail.com>
2020-04-08 09:56:55 +0300

    exceptions

--- ExceptionsJR/MultipleExceptions/ExceptionCatch.java
+++ ExceptionsJR/MultipleExceptions/ExceptionCatch.java
@@ -1,8 +1,31 @@
 package bat.coding.java.ExceptionsJR.MultipleExceptions;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+
 public class ExceptionCatch {
     public static void main(String[] args) {
 
+       try {
+           BufferedReader bufferedReader = new BufferedReader(new FileReader("hgjkbk"));
+       } catch (FileNotFoundException e) {
+           System.out.println("FILE NOT FOUND EXCEPTION CAUGHT");
+       }
+
+       try {
+           HashMap<String,String> hashMap = new HashMap<>(null);
+       } catch (NullPointerException e) {
+           System.out.println("NULL POINTER EXCEPTION CAUGHT");
+       }
+
+        try {
+            System.out.println(4 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("ARITHMETIC EXCEPTION CAUGHT");
+            e.printStackTrace();
+        }
     }
 }
 
