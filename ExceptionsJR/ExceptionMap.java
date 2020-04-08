f4982a931b87fe434b5296985b55af9a7e5ecc0a
katemalets <katyamask@gmail.com>
2020-04-08 08:23:43 +0300

    exceptions

---
+++ ExceptionsJR/ExceptionMap.java
@@ -0,0 +1,16 @@
+package bat.coding.java.ExceptionsJR;
+
+import java.util.HashMap;
+
+public class ExceptionMap {
+    public static void main(String[] args) {
+        try {
+            HashMap<String, String> map = new HashMap<>(null);
+            map.put(null, null);
+            map.remove(null);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("NULLNULLNULL");
+        }
+    }
+}
