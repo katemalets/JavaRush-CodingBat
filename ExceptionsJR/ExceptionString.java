8df037fdc0bef24c2c131ac6af64851e117662e5
katemalets <katyamask@gmail.com>
2020-04-08 08:10:29 +0300

    exceptions

--- ExceptionsJR/ExceptionString.java
+++ ExceptionsJR/ExceptionString.java
@@ -2,11 +2,13 @@ package bat.coding.java.ExceptionsJR;
 
 public class ExceptionString {
     public static void main(String[] args) {
+        String m = null;
         try {
             String s = null;
-            String m = s.toLowerCase();
+            m = s.toLowerCase();
         } catch (NullPointerException e){
             System.out.println("Mistake is noticed!");
+            System.out.println(m);
         }
     }
 }
