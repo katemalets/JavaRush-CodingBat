6232d764886f8801784edbff7817d399d2eaa60b
katemalets <katyamask@gmail.com>
2020-04-08 11:32:14 +0300

    tokenizer

--- StringsJR/StringTokenizerExample.java
+++ StringsJR/StringTokenizerExample.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 
 public class StringTokenizerExample {
     public static void main(String[] args) {
+
         StringTokenizer stringTokenizer = new StringTokenizer("query cat dog"," ");
         String token;
         while (stringTokenizer.hasMoreTokens()){
